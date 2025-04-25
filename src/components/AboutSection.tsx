5a0feade3194afbfdd133ae238c9e7b6ac8759bf
Varshaa <vvarshaas@gmail.com>
2025-04-25 19:39:59 +0530

    animations for AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client";
+
+import React, { useRef } from 'react'
 import About from './About'
 import Button from './Button'
 import WorkflowCard from './WorkflowCard'
 import CTASection from './CTASection'
 import FooterSection from './FooterSection'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+gsap.registerPlugin(useGSAP);
 
 function AboutSection() {
+
+  const aboutTextRef = useRef<HTMLDivElement | null>(null);
+  const aboutTitleRef = useRef<HTMLDivElement | null>(null);
+  const aboutButtonRef = useRef<HTMLDivElement | null>(null);
+  const workflowRef = useRef<HTMLDivElement | null>(null);
 
   const cardArray = [
     { title: "Connect",
@@ -22,9 +33,120 @@ function AboutSection() {
     }
   ]
 
+  useGSAP(() => {
+
+    if(!aboutTextRef.current || !aboutTitleRef.current || !aboutButtonRef.current) return;
+
+    Array.from(aboutTextRef.current.children).forEach((child) => {
+      gsap.from(child, {
+        opacity: 0,
+          autoAlpha: 1,
+          y: 50,
+          ease: "power1.out",
+          scrollTrigger: {
+            trigger: child,
+            start: "top 80%",
+          },
+      })
+    })
+
+    const ctxAbout = gsap.context(() => {
+
+      gsap.from(".word", {
+        opacity: 0,
+        autoAlpha: 1,
+        stagger: 0.15,
+        y: 50,
+        clipPath: "inset(0% 0% 100% 0%)",
+        scrollTrigger: {
+          trigger: aboutTitleRef.current,
+          start: "top 90%"
+        }
+      })
+      
+    }, aboutTitleRef)
+
+    gsap.from(aboutButtonRef.current, {
+      opacity: 0,
+      autoAlpha: 1,
+      y: 50,
+      scrollTrigger: {
+        trigger: aboutButtonRef.current,
+        start: "top 80%"
+      }
+    })
+
+    const aboutMM = gsap.matchMedia();
+
+    aboutMM.add("(max-width: 380px)", () => {
+
+      if(!workflowRef.current) return;
+
+      Array.from(workflowRef.current.children).forEach((child) => {
+        gsap.from(child, {
+          opacity: 0,
+          autoAlpha: 1,
+          y: 50,
+          duration: 0.5,
+          ease: "power1.out",
+          scrollTrigger: {
+            trigger: child,
+            start: "top 80%",
+          },
+        });
+      });
+    })
+
+    aboutMM.add("(min-width: 381px) and (max-width: 1023px)", () => {
+
+      if(!workflowRef.current) return;
+
+      Array.from(workflowRef.current.children).forEach((child) => {
+        gsap.from(child, {
+          opacity: 0,
+          autoAlpha: 1,
+          y: 50,
+          duration: 0.5,
+          ease: "power1.out",
+          scrollTrigger: {
+            trigger: child,
+            start: "top 80%",
+          }
+        })
+      })
+    })
+
+    aboutMM.add("(min-width: 1024px)", () => {
+
+      if(!workflowRef.current) return;
+
+      gsap.from(workflowRef.current.children, {
+        opacity: 0,
+        autoAlpha: 1,
+        y: 100,
+        stagger: 0.15,
+        duration: 0.5,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: workflowRef.current,
+          start: "top 70%"
+        }
+      })
+
+    })
+
+    return () => {
+      ctxAbout.revert();
+    }
+
+  }, []);
+
   return (
     <div className='bg-primary-blue rounded-t-3xl pb-16 px-6 pt-28 md:px-10 lg:pt-52 lg:px-24'>
-      <div className='flex flex-col pb-20 space-y-16 xs:space-y-24 sm:pb-32 md:pb-60 lg:space-y-96 lg:pb-80 xl:pb-96'>
+      <div 
+        ref={aboutTextRef}
+        className='flex flex-col pb-20 space-y-16 xs:space-y-24 sm:pb-32 md:pb-60 lg:space-y-96 lg:pb-80 xl:pb-96'
+      >
         <About
           title='Client and stakeholder consideration.'
           text="Experienced business leadership that's driven by fair outcomes."
@@ -35,17 +157,36 @@ function AboutSection() {
         />
       </div>
       <div className='flex flex-col justify-between space-y-8 pb-14 lg:flex-row lg:items-end lg:space-y-0'>
-        <div className='text-white font-bold text-4xl tracking-wide leading-12 xs:w-[65vw] sm:w-[55vw] sm:leading-14 md:w-[43vw] lg:text-5xl lg:w-[55vw] lg:leading-20 lg:tracking-wider lg:text-[64px]'>
-          Powering your workplace journey
+        <div 
+          ref={aboutTitleRef}
+          className='text-white font-bold text-4xl tracking-wide leading-12 xs:w-[65vw] sm:w-[55vw] sm:leading-14 md:w-[43vw] lg:text-5xl lg:w-[55vw] lg:leading-20 lg:tracking-wider lg:text-[64px]'
+        >
+          {`Powering your workplace journey`
+          .split("\n").map((line, index) => (
+            <div key={index}>
+              {line.split(" ").map((word, i) => (
+                <span
+                  key={i} 
+                  className='word inline-block mr-4'
+                >
+                  {word}
+                </span>
+              ))}
+            </div>
+          ))
+          }
         </div>
-        <div className='w-fit'>
+        <div 
+          ref={aboutButtonRef}
+          className='w-fit'
+        >
           <Button
             variant='secondary'
             text='Our approach'
           />
         </div>
       </div>
-      <div className='grid grid-cols-1 gap-y-4 gap-x-6 pb-32 xs:pb-44 md:grid-cols-2 lg:gap-y-0 lg:grid-cols-4'>
+      <div ref={workflowRef} className='grid grid-cols-1 gap-y-4 gap-x-6 pb-32 xs:pb-44 md:grid-cols-2 lg:gap-y-0 lg:grid-cols-4'>
         {cardArray.map((card, index) => (
           <WorkflowCard
             {...card}
