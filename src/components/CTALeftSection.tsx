849922c099214fb6de01e78f59f10e08ba4db2eb
Varshaa <vvarshaas@gmail.com>
2025-04-28 10:52:18 +0530

    animations for CTASection

--- src/components/CTALeftSection.tsx
+++ src/components/CTALeftSection.tsx
@@ -1,20 +1,82 @@
-import React from 'react'
+"use client";
+
+import React, { useRef } from 'react'
 import Button from './Button'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+gsap.registerPlugin(useGSAP);
 
 function CTALeftSection() {
+
+  const ctaLeftRef = useRef<HTMLDivElement | null>(null);
+  const ctaLeftTextRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+
+    if(!ctaLeftRef.current || !ctaLeftTextRef.current) return;
+    
+    gsap.from(ctaLeftRef.current, {
+      opacity: 0,
+      autoAlpha: 1,
+      y: 100,
+      scrollTrigger: {
+        trigger: ctaLeftRef.current,
+        start: "top 90%"
+      }
+    })
+
+    const leftCtaCtx = gsap.context(() => {
+
+      gsap.from(".word", {
+        opacity: 0,
+        autoAlpha: 1,
+        y: 50,
+        stagger: 0.15,
+        clipPath: "inset(0% 0% 100% 0%)",
+        scrollTrigger: {
+          trigger: ctaLeftRef.current,
+          start: "top 80%"
+        }
+      })
+
+    }, ctaLeftTextRef) 
+
+    return () => leftCtaCtx.revert();
+
+  }, []);
+
   return (
-    <div className="bg-[url('/images/CTA-img.webp')] bg-cover bg-no-repeat bg-center relative px-10 rounded-3xl h-[350px] pt-16 pb-10 lg:w-[40vw] lg:h-[650px] xl:h-[550px]">
-        <div className='absolute inset-0 bg-black opacity-40 rounded-3xl'></div>
-        <div className='relative z-10 flex flex-col space-y-8'>
-            <div className='text-white text-[32px] font-bold tracking-wide leading-10 sm:w-[40vw] md:w-[50vw] md:text-[40px] md:leading-14 lg:text-5xl lg:w-auto'>
-                Want to join our
-                experienced team?
-            </div>
-            <Button
-                variant='primary'
-                text='Join the team'
-            />
-        </div>
+    <div 
+      ref={ctaLeftRef} 
+      className="bg-[url('/images/CTA-img.webp')] bg-cover bg-no-repeat bg-center relative px-10 rounded-3xl h-[350px] pt-16 pb-10 lg:w-[40vw] lg:h-[650px] xl:h-[550px]"
+    >
+      <div className='absolute inset-0 bg-black opacity-40 rounded-3xl'></div>
+      <div className='relative z-10 flex flex-col space-y-8'>
+          <div 
+            ref={ctaLeftTextRef}
+            className='text-white text-[32px] font-bold tracking-wide leading-10 sm:w-[40vw] md:w-[50vw] md:text-[40px] md:leading-14 lg:text-5xl lg:w-auto'
+          >
+            {`Want to join our experienced team?`
+              .split("\n").map((line, index) => (
+                <div key={index}>
+                  {line.split(" ").map((word, i) => (
+                    <span
+                      key={i}
+                      className='word inline-block mr-4'
+                    >
+                      {word}
+                    </span>
+                  ))}
+                </div>
+              ))
+            }
+          </div>
+          <Button
+              variant='primary'
+              text='Join the team'
+          />
+      </div>
     </div>
   )
 }
--- src/components/CTARightSection.tsx
+++ src/components/CTARightSection.tsx
@@ -1,14 +1,80 @@
-import React from 'react'
+"use client";
+
+import React, { useRef } from 'react'
 import CTAIcon from './CTAIcon'
 import Button from './Button'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+gsap.registerPlugin(useGSAP);
 
 function CTARightSection() {
+
+    const ctaRightRef = useRef<HTMLDivElement | null>(null);
+    const ctaRightTextRef = useRef<HTMLDivElement | null>(null);
+
+    useGSAP(( ) => {
+
+        if(!ctaRightRef.current || !ctaRightTextRef.current) return;
+        
+        gsap.from(ctaRightRef.current, {
+            opacity: 0,
+            autoAlpha: 1,
+            y: 100,
+            delay: 0.2,
+            scrollTrigger: {
+              trigger: ctaRightTextRef.current,
+              start: "top 90%"
+            }
+          })
+
+        const rightCtaCtx = gsap.context(() => {
+
+        gsap.from(".word", {
+            opacity: 0,
+            autoAlpha: 1,
+            y: 50,
+            stagger: 0.15,
+            delay: 0.2,
+            clipPath: "inset(0% 0% 100% 0%)",
+            scrollTrigger: {
+            trigger: ctaRightRef.current,
+            start: "top 80%"
+            }
+        })
+    
+        }, ctaRightTextRef) 
+    
+        return () => rightCtaCtx.revert();
+
+    }, []);
+
   return (
-    <div className='relative bg-primary-yellow text-primary-blue rounded-3xl flex py-16 px-4 h-[500px] xs:px-16 xs:h-[600px] sm:h-[550px] lg:w-[50vw] lg:h-[650px] xl:h-[550px]'>
+    <div 
+        ref={ctaRightRef}
+        className='relative bg-primary-yellow text-primary-blue rounded-3xl flex py-16 px-4 h-[500px] xs:px-16 xs:h-[600px] sm:h-[550px] lg:w-[50vw] lg:h-[650px] xl:h-[550px]'
+    >
         <div className='flex flex-col space-y-6'>
-            <div className=' font-bold tracking-wide leading-12 text-[32px] w-80 md:w-96 md:text-[40px] lg:leading-14 xl:leading-16 lg:w-60 lg:text-5xl xl:w-[450px]'>
-                Ready to chat?
-                Start your journey.
+            <div 
+                ref={ctaRightTextRef}
+                className='font-bold tracking-wide leading-12 text-[32px] w-80 md:w-96 md:text-[40px] lg:leading-14 xl:leading-16 lg:w-60 lg:text-5xl xl:w-[450px]'
+            >
+                {`Ready to chat? Start your journey.`
+                .split("\n").map((line, index) => (
+                    <div key={index}>
+                        {line
+                        .split(" ").map((word, i) => (
+                            <span
+                                key={i}
+                                className='word inline-block mr-4'
+                            >
+                                {word}
+                            </span>
+                        ))
+                        }
+                    </div>
+                ))
+                }
             </div>
             <Button
                 variant='primary'
