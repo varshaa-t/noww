939d14ad7368bced2a01d3f9da2badef35586baf
Varshaa <vvarshaas@gmail.com>
2025-04-28 11:22:31 +0530

    animations for FooterSection

--- public/icons/LinkedIn.tsx
+++ public/icons/LinkedIn.tsx
@@ -2,7 +2,7 @@ import { RxLinkedinLogo } from "react-icons/rx";
 
 function LinkedIn() {
   return (
-    <RxLinkedinLogo className="text-primary-yellow text-2xl"/>
+    <RxLinkedinLogo className="text-primary-yellow text-2xl hover:cursor-pointer"/>
 )
 }
 
--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+"use client";
+
+import React, { useRef } from 'react'
 import LinkedIn from '../../public/icons/LinkedIn'
 import FooterIcon from './FooterIcon'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 function FooterSection() {
+
+  const footerRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+
+    if(!footerRef.current) return;
+
+    gsap.from(footerRef.current.children, {
+      opacity: 0,
+      autoAlpha: 1,
+      stagger: 0.15,
+      y: 100, 
+      scrollTrigger: {
+        trigger: footerRef.current,
+        start: "top 70%"
+      }
+    })
+
+  }, []);
+
   return (
-    <div>
+    <div
+      ref={footerRef}
+    >
         <div className='flex justify-center'>
           <div className='border-b border-primary-yellow pb-20'>
             <FooterIcon/>
@@ -14,9 +40,9 @@ function FooterSection() {
           <div className='flex justify-between mx-[2vw] xs:mx-[4vw] sm:mx-[3vw] lg:-mx-[1vw] xl:mx-[0.5vw] 2xl:mx-[1vw] pt-8'>
               <div className='flex text-primary-yellow font-primary-light space-x-4 lg:space-x-6 2xl:space-x-8'>
                 <div className='text-[14px] hidden lg:block'>© 2025 NOWW Advisory. All rights reserved.</div>
-                <div className='text-[14px]'>Privacy Policy</div>
-                <div className='text-[14px]'>Terms of Use</div>
-                <div className='text-[14px]'>Site by N4</div>
+                <div className='text-[14px] hover:underline hover:underline-offset-4 hover:cursor-pointer'>Privacy Policy</div>
+                <div className='text-[14px] hover:underline hover:underline-offset-4 hover:cursor-pointer'>Terms of Use</div>
+                <div className='text-[14px] hover:underline hover:underline-offset-4 hover:cursor-pointer'>Site by N4</div>
               </div>
               <div className='hidden xs:block'>
                 <LinkedIn/>
